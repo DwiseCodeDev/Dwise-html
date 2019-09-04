b2657d176dea9e3c794995557edf190eb087868f
Dennis Etiwe <dennisetiwe@gmail.com>
2019-09-04 16:03:03 +0100

    --- Auto-Git Commit ---

---
+++ StartNG/contactform.js
@@ -0,0 +1,37 @@
+<script> 
+function Contact Form()								 
+{ 
+	var name = document.forms["ContactForm"]["Name"];			 
+	var email = document.forms["ContactForm"]["EMail"];  
+
+	if (name.value == "")								 
+	{ 
+		window.alert("Please enter your name."); 
+		name.focus(); 
+		return false; 
+	} 
+
+	
+	if (email.value == "")								 
+	{ 
+		window.alert("Please enter a valid e-mail address."); 
+		email.focus(); 
+		return false; 
+	} 
+
+	if (email.value.indexOf("@", 0) < 0)				 
+	{ 
+		window.alert("Please enter a valid e-mail address."); 
+		email.focus(); 
+		return false; 
+	} 
+
+	if (email.value.indexOf(".", 0) < 0)				 
+	{ 
+		window.alert("Please enter a valid e-mail address."); 
+		email.focus(); 
+		return false; 
+	} 
+
+	return true; 
+}</script> 
